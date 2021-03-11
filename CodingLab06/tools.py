2b0184ebc67d4c3e9843bc39f8a4f17501ead5a6
Katie Savard <k8tsavard@gmail.com>
2021-03-10 22:40:33 -0500

    finished lab

--- CodingLab06/tools.py
+++ CodingLab06/tools.py
@@ -13,11 +13,14 @@ def picky(num):
 def load_dat(filename):
     data_array = pd.read_table(filename,sep=',', header=None)
     time = data_array[0].to_numpy()
-    t_0 = np.min(time)
+    t_0 = 1973
     time = time-t_0 #set smallest time to be t=0
     sidereal_day = 366.242
     time = time*sidereal_day #convert to sidereal day
     s = data_array[1].to_numpy()
     err = data_array[2].to_numpy()
+    seconds = 86164.1
+    s = s/seconds
+    err = err/seconds
     return time , s , err
 
